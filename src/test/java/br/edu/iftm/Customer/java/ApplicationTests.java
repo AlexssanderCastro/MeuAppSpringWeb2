b06b600d671fa0a9236ea357bf02a3c6666a36a1
alexssander77 <alexssandercastro1@gmail.com>
2025-09-14 16:17:43 -0300

    Initial commit

---
+++ src/test/java/br/edu/iftm/Customer/java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.Customer.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
